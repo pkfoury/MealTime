8d7ffbeda2184024f56b610e210797db99f7b944
sjrichardson <samrichardson812@gmail.com>
2018-10-03 00:38:06 -0400

    add migrations to link user-recipes, ingredient-allergens, user-budgets

--- MealTime/db/schema.rb
+++ MealTime/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_203937) do
+ActiveRecord::Schema.define(version: 2018_10_03_042934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2018_10_02_203937) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "allergens_ingredients", id: false, force: :cascade do |t|
+    t.bigint "ingredient_id", null: false
+    t.bigint "allergen_id", null: false
+    t.index ["allergen_id"], name: "index_allergens_ingredients_on_allergen_id"
+    t.index ["ingredient_id"], name: "index_allergens_ingredients_on_ingredient_id"
+  end
+
   create_table "budgets", force: :cascade do |t|
     t.decimal "weeklyBudget", default: "0.0", null: false
     t.decimal "weeklySpending", default: "0.0", null: false
@@ -28,6 +35,8 @@ ActiveRecord::Schema.define(version: 2018_10_02_203937) do
     t.decimal "monthlySpending", default: "0.0", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_budgets_on_user_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -57,6 +66,8 @@ ActiveRecord::Schema.define(version: 2018_10_02_203937) do
     t.text "creatorComments", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "restaurant_histories", force: :cascade do |t|
@@ -92,6 +103,8 @@ ActiveRecord::Schema.define(version: 2018_10_02_203937) do
     t.boolean "vote", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "recipe_id"
+    t.index ["recipe_id"], name: "index_user_votes_on_recipe_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -103,4 +116,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_203937) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "budgets", "users"
+  add_foreign_key "recipes", "users"
+  add_foreign_key "user_votes", "recipes"
 end
