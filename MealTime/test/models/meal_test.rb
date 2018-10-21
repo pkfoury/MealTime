66633f3bd0ccbe938e843b4012738ff6fce5e643
sjrichardson <samrichardson812@gmail.com>
2018-10-21 15:02:06 -0400

    work on implementing the join with meal and user on the model side

--- MealTime/test/models/meal_test.rb
+++ MealTime/test/models/meal_test.rb
@@ -4,4 +4,7 @@ class MealTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup:
+    @meal Meal.new(:dateOfMeal = "9/18/18", :user_id = 1, :recipe_id = 1)
+  end
 end
