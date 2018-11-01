bc131f4cdea7b0d09a3d9df8f66218c128e252d9
sjrichardson <samrichardson812@gmail.com>
2018-11-01 15:31:28 -0400

    add test fixtures and change test database in database.yml

--- MealTime/test/models/meal_test.rb
+++ MealTime/test/models/meal_test.rb
@@ -5,10 +5,10 @@ class MealTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @meal = Meal.new(date_of_meal: "9/18/18", user_id: 1, recipe_id: 1)
+
   end
 
   test "should have meal associated with user" do
-    assert_not @meal.users.empty?
+    flunk("hello there")
   end
 end
