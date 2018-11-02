4619c3fb1694d206fbfbc975c17b309c5382445e
Sam Richardson <samrichardson812@gmail.com>
2018-11-02 03:43:18 -0400

    Sam sprint2 (#62)
    * Add UserVote and associate it with User model
    
    * Undo previous changes, create user_vote migration
    
    * Create recipes, budgets, and user_votes tables
    
    * Remove altered code from user class
    
    * correct DB implementation issues
    
    * add migrations to link user-recipes, ingredient-allergens, user-budgets
    
    * Link user_locations with user,  restaurant_hitsories with user and restaurant
    
    * add meal table and link it to user and recipe table
    
    * link up models
    
    * add RecipesController index
    
    * package change commit
    
    minor commit
    
    * implement show in recipes_controller
    
    * push for help
    
    * finish recipe controller
    
    * rename dateOfMeal column in meals
    
    * cascade delete all information associated with user
    
    * implement meals show method
    
    * finish meals controller
    
    * implement user_votes_controller
    
    * allow user to dislike a restaurant in DB
    
    * add model realtaionship between users and meals
    
    * work on implementing the join with meal and user on the model side
    
    * fix some of the issues with DB constraints
    
    * fix all relationships from user class
    
    * minor db model changes
    
    * fix some relationship issues and add ingredients_meals table
    
    * remove ingredients_meals and create ingredients_recipes join table
    
    * implement has_many-through relationships in join tables and create respective models
    
    * add test fixtures and change test database in database.yml
    
    * add migration to add public field in the recipes table
    
    * add meals_ingredients model
    
    * rename meals_ingredients model
    
    * fix issues with tables
    
    * attempt to fix testing error
    
    * add nutrition to recipes
    
    * add user_allergens

--- MealTime/app/models/restaurant_preference.rb
+++ MealTime/app/models/restaurant_preference.rb
@@ -1,4 +1,8 @@
 class RestaurantPreference < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  def changepreference(restaurant_id, preference)
+    self.where(:restaurant_id = restaurant_id).update_attribute(:dislike preference)
+  end
 end
