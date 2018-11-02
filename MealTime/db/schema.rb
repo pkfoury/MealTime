c029383aeb49a9f307a6fde1791603f4a02603f1
sjrichardson <samrichardson812@gmail.com>
2018-11-01 22:12:42 -0400

    add migration to add public field in the recipes table

--- MealTime/db/schema.rb
+++ MealTime/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_184259) do
+ActiveRecord::Schema.define(version: 2018_11_01_211903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 2018_11_01_184259) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.boolean "public", default: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
