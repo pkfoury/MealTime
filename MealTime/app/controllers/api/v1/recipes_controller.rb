a0424674377e5b366cb8cef7054b9051f99de0a7
sjrichardson <samrichardson812@gmail.com>
2018-11-29 15:09:19 -0500

    allow recipes_controller to sort out recipes containing a given allergen

--- MealTime/app/controllers/api/v1/recipes_controller.rb
+++ MealTime/app/controllers/api/v1/recipes_controller.rb
@@ -30,7 +30,36 @@ module Api
                 render json: {status: 'SUCCESS', message: 'Searching for recipes with filters.'}, status: :ok
             end
             def recipe_allergens
-                recipe = Recipe.joins(ingredients_recipes: {ingredients: {allergens_ingredients: :allergen }}).where.not(allergens: {name: params[:allergen_name]})
+                recipe = []
+                records_array = ActiveRecord::Base.connection.execute(%"
+                    SELECT 
+                        recipes.recipe_name, 
+                        recipes.instructions,
+                        recipes.cook_time,
+                        recipes.creator_comments,
+                        recipes.total_calories,
+                        recipes.total_fat,
+                        recipes.total_trans_fat,
+                        recipes.total_cholesterol,
+                        recipes.total_sodium,
+                        recipes.total_carbs,
+                        recipes.total_protein,
+                        recipes.difficulty 
+                    FROM recipes
+                        INNER JOIN ingredients_recipes 
+                            ON (recipes.id = ingredients_recipes.recipe_id)
+                        INNER JOIN ingredients 
+                            ON (ingredients_recipes.ingredient_id = ingredients.id)
+                        INNER JOIN allergens_ingredients 
+                            ON (ingredients.id = allergens_ingredients.ingredient_id)
+                        INNER JOIN allergens 
+                            ON (allergens_ingredients.allergen_id = allergens.id)
+                    WHERE allergens.name != '#{params[:allergen_name]}'")
+                
+                records_array.each do |row|
+                    recipe << add_to_recipe(row)
+                end
+                # recipe = Recipe.joins(ingredients_recipes: {ingredients: {allergens_ingredients: :allergen }}).where.not(allergens: {name: params[:allergen_name]})
                 render json: {status: 'SUCCESS', message: 'Searching for recipes that do not contain specified allergen', data: recipe}, status: :ok
             end
 
@@ -52,6 +81,22 @@ module Api
             private 
             def recipe_params
                 params.permit(:user_id, :recipe_name, :instructions, :cook_time,:creator_comments, :allergen_name)
+            end
+            def add_to_recipe(row)
+                temp = Recipe.new()
+                temp.recipe_name = row[0]
+                temp.instructions = row[1]
+                temp.cook_time = row[2]
+                temp.creator_comments = row[3]
+                temp.total_calories = row[4]
+                temp.total_fat = row[5]
+                temp.total_trans_fat = row[6]
+                temp.total_cholesterol = row[7]
+                temp.total_sodium = row[8]
+                temp.total_carbs = row[9]
+                temp.total_protein = row[10]
+                temp.difficulty = row[11]
+                return temp
             end
         end
     end
