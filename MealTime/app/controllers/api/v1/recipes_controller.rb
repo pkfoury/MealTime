0d24bddfd38a554adcf1cc14643a275e04672a05
Sam Richardson <samrichardson812@gmail.com>
2018-11-29 16:07:41 -0500

    Sam sprint3 (#91)
    * fix data types for macros
    
    * add timestamps to join tables
    
    * drop restaurants model and remove references to it
    
    * remove reference to restaurant in restaurant_history
    
    * create allergens_controller and implement index and create
    
    * add validations to recipe and remove update methods
    
    * add validations to meal and remove update methods
    
    * remove update methods completely from recipe
    
    * add restaurant_dislikes table
    
    * remove update methods for meal
    
    * add allergen filter to recipes_controller
    
    * remove token check from recipes controller
    
    * saving to merge changes
    
    * allow recipes_controller to sort out recipes containing a given allergen
    
    * allergen filter can handle individual or mulitple allergens

--- MealTime/app/controllers/api/v1/recipes_controller.rb
+++ MealTime/app/controllers/api/v1/recipes_controller.rb
@@ -31,30 +31,12 @@ module Api
             end
             def recipe_allergens
                 recipe = []
-                records_array = ActiveRecord::Base.connection.execute(%"
-                    SELECT 
-                        recipes.recipe_name, 
-                        recipes.instructions,
-                        recipes.cook_time,
-                        recipes.creator_comments,
-                        recipes.total_calories,
-                        recipes.total_fat,
-                        recipes.total_trans_fat,
-                        recipes.total_cholesterol,
-                        recipes.total_sodium,
-                        recipes.total_carbs,
-                        recipes.total_protein,
-                        recipes.difficulty 
-                    FROM recipes
-                        INNER JOIN ingredients_recipes 
-                            ON (recipes.id = ingredients_recipes.recipe_id)
-                        INNER JOIN ingredients 
-                            ON (ingredients_recipes.ingredient_id = ingredients.id)
-                        INNER JOIN allergens_ingredients 
-                            ON (ingredients.id = allergens_ingredients.ingredient_id)
-                        INNER JOIN allergens 
-                            ON (allergens_ingredients.allergen_id = allergens.id)
-                    WHERE allergens.name != '#{params[:allergen_name]}'")
+                #remove [] if in the string
+                params[:allergen_name].tr!('[]','')
+                #split input string by ',' if a list of allergens are provided
+                allergen_list = params[:allergen_name].split(',')
+                
+                records_array = ActiveRecord::Base.connection.execute(build_query(allergen_list))
                 
                 records_array.each do |row|
                     recipe << add_to_recipe(row)
@@ -81,6 +63,42 @@ module Api
             private 
             def recipe_params
                 params.permit(:user_id, :recipe_name, :instructions, :cook_time,:creator_comments, :allergen_name)
+            end
+            #formats the query with the allergen_list allergens
+            def build_query(allergen_list) 
+                query = %"SELECT 
+                    recipes.recipe_name, 
+                    recipes.instructions,
+                    recipes.cook_time,
+                    recipes.creator_comments,
+                    recipes.total_calories,
+                    recipes.total_fat,
+                    recipes.total_trans_fat,
+                    recipes.total_cholesterol,
+                    recipes.total_sodium,
+                    recipes.total_carbs,
+                    recipes.total_protein,
+                    recipes.difficulty 
+                FROM recipes
+                    INNER JOIN ingredients_recipes 
+                        ON (recipes.id = ingredients_recipes.recipe_id)
+                    INNER JOIN ingredients 
+                        ON (ingredients_recipes.ingredient_id = ingredients.id)
+                    INNER JOIN allergens_ingredients 
+                        ON (ingredients.id = allergens_ingredients.ingredient_id)
+                    INNER JOIN allergens 
+                        ON (allergens_ingredients.allergen_id = allergens.id)
+                WHERE allergens.name != '#{allergen_list[0]}'"
+                #concat rest of allergens into the query
+                query << add_allergens_to_query(allergen_list[1..-1])
+            end
+            #if allergen_list contains multiple allergens concat them on to the query
+            def add_allergens_to_query(allergen_list)
+                ret_string = ''
+                allergen_list.each do |allergen|
+                    ret_string << %" AND allergens.name != '#{allergen}'"
+                end
+                return ret_string
             end
             def add_to_recipe(row)
                 temp = Recipe.new()
