5a4995fb0dbaf11fc25eb0a3149253d5ef1673a7
sjrichardson <samrichardson812@gmail.com>
2018-11-30 14:07:34 -0500

    fix routing issues brought in earlier commit

--- MealTime/app/controllers/api/v1/recipes_controller.rb
+++ MealTime/app/controllers/api/v1/recipes_controller.rb
@@ -2,7 +2,7 @@ module Api
     module V1
         class RecipesController < ApplicationController
             skip_before_action :verify_authenticity_token
-            # skip_before_action :require_token
+            #  skip_before_action :require_token
             def index
                 recipe = Recipe.order("recipe_name DESC")
                 render json: {status: 'SUCCESS', message: 'Pulling all recipes desc.', data:recipe}, status: :ok
