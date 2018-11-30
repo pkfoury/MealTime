e61cc3411be88019b0dc31e3a8ec2a456904c1fb
sjrichardson <samrichardson812@gmail.com>
2018-11-29 21:21:59 -0500

    comment out skip token

--- MealTime/db/seeds.rb
+++ MealTime/db/seeds.rb
@@ -1,6 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+
+# any of these values that you uncomment will be added to the DB regardless
+# of if they were in it or not. Keep our DB clean, only uncomment seeds you 
+# want to be added and comment out the rest
+
+#-------------------------------------------------------------------------------
+
 # @meal_type1 = MealType.new(name: "Breakfast")
 # @meal_type2 = MealType.new(name: "Lunch")
 # @meal_type3 = MealType.new(name: "Dinner")
